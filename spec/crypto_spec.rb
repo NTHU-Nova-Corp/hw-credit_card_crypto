89f3d6498a300963427780809e3114e26653f37b
sarunyu.sst <sarunyu.sst@gmail.com>
2022-03-13 18:35:25 +0800

    Revert "Ernesto: Added more test cases for verifying plaintext with spaces"
    This reverts commit a17882970da6b8432dbbe29f53288be7cf822cda.

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -7,7 +7,6 @@ require 'minitest/autorun'
 
 describe 'Test card info encryption' do
   before do
-    @text_with_space = "    Hello, how are you?   "
     @cc = CreditCard.new('4916603231464963', 'Mar-30-2020',
                          'Soumya Ray', 'Visa')
     @key = 3
@@ -25,12 +24,6 @@ describe 'Test card info encryption' do
       dec = SubstitutionCipher::Caesar.decrypt(enc, @key)
       _(dec).must_equal @cc.to_s
     end
-
-    it 'should decrypt text with spaces correctly' do
-      enc = SubstitutionCipher::Caesar.encrypt(@text_with_space, @key)
-      dec = SubstitutionCipher::Caesar.decrypt(enc, @key)
-      _(dec).must_equal @text_with_space
-    end
   end
 
   describe 'Using Permutation cipher' do
@@ -44,12 +37,6 @@ describe 'Test card info encryption' do
       enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
       dec = SubstitutionCipher::Permutation.decrypt(enc, @key)
       _(dec).must_equal @cc.to_s
-    end
-
-    it 'should decrypt text with spaces correctly' do
-      enc = SubstitutionCipher::Permutation.encrypt(@text_with_space, @key)
-      dec = SubstitutionCipher::Permutation.decrypt(enc, @key)
-      _(dec).must_equal @text_with_space.to_s
     end
   end
 
@@ -66,12 +53,6 @@ describe 'Test card info encryption' do
       enc = DoubleTranspositionCipher.encrypt(@cc, @key)
       dec = DoubleTranspositionCipher.decrypt(enc, @key)
       _(dec).must_equal @cc.to_s
-    end
-
-    it 'should decrypt text with spaces correctly' do
-      enc = DoubleTranspositionCipher.encrypt(@text_with_space, @key)
-      dec = DoubleTranspositionCipher.decrypt(enc, @key)
-      _(dec).must_equal @text_with_space
     end
   end
 
